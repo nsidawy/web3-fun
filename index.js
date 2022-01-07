b87eb9c5906acaf06a3d0b522eee01e6903d05c0
Nick Sidawy <nick.sidawy@gmail.com>
2022-01-07 00:10:58 -0500

    multisig token minting

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const dotenv = require("dotenv")
 const S = require('@emurgo/cardano-serialization-lib-nodejs/cardano_serialization_lib.js')
 
 const app = express();
-const port = process.env.PORT;
-const vendingAddress = process.env.VENDING_ADDRESS;
+
+const PORT = process.env.PORT;
+const VENDING_ADDRESS_BECH32 = process.env.VENDING_ADDRESS_BECH32;
+const MINTING_ADDRESS_BECH32 = process.env.MINTING_ADDRESS_BECH32;
+// Value retrieved by using "cardano-cli address key-hash"
+const MINTING_KEY_HASH = process.env.MINTING_KEY_HASH;
+// Getting this value requires:
+//  * Getting the CBOR hex value out of the *skey file
+//  * Dropping the first 4 characters "5820"
+//  * Taking the output of "echo <remaing CBOR hex values> | bech32 ed25519_sk"
+const MINTING_SKEY_BECH32 = process.env.MINTING_SKEY_BECH32;
 
 app.use(bodyParser.json());
 app.use(express.static('test'));
@@ -17,7 +26,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/vendingAddress', (req, res) => {
-    res.send(vendingAddress);
+    res.send(VENDING_ADDRESS_BECH32);
 });
 
 app.post('/getBech32Address', (req, res) => {
@@ -39,13 +48,12 @@ function getTxConfig() {
 
 app.post('/payVendingMachine', (req, res) => {
     const txBuilder =  S.TransactionBuilder.new(getTxConfig());
-    const utxos = req.body.utxosHex.map(u => S.TransactionUnspentOutput.from_bytes(Buffer.from(u, "hex")))
     const lovelace = req.body.lovelace;
 
     const outputs = S.TransactionOutputs.new();
     outputs.add(
         S.TransactionOutput.new(
-            S.Address.from_bech32(vendingAddress),
+            S.Address.from_bech32(VENDING_ADDRESS_BECH32),
             S.Value.new(S.BigNum.from_str(lovelace.toString()))
         )
     );
@@ -77,7 +85,7 @@ app.post('/dip', (req, res) => {
     assets.insert(sauceAssetName, S.BigNum.from_str("1"));
     multiAsset.insert(policyScriptHash, assets);
     txBuilder.add_output_coin_and_asset(
-        S.Address.from_bech32(vendingAddress),
+        S.Address.from_bech32(VENDING_ADDRESS_BECH32),
         S.BigNum.from_str(lovelace.toString()),
         multiAsset);
 
@@ -93,6 +101,50 @@ app.post('/dip', (req, res) => {
     res.send(transactionBytes)
 });
 
+function getMintingNativeScript() {
+    const mintNativeScript = S.NativeScript.new_script_pubkey(S.ScriptPubkey.new(
+        S.Ed25519KeyHash.from_bytes(Buffer.from(MINTING_KEY_HASH, "hex"))));
+    //const mintNativeScript = S.NativeScript.new_timelock_start(S.TimelockStart.new(0));
+    return mintNativeScript;
+}
+
+app.post('/buyEpochCoins', (req, res) => {
+    const txBuilder =  S.TransactionBuilder.new(getTxConfig());
+    const epochCoinAmount = parseInt(req.body.epochCoinAmount)
+    const lovelace = 5000000 * epochCoinAmount;
+
+    const mintingAddress = S.Address.from_bech32(MINTING_ADDRESS_BECH32);
+    const buyerAddress = S.Address.from_bytes(Buffer.from(req.body.address, 'hex'));
+
+    // required for native script that requires coins minted after 0 slot
+    txBuilder.set_validity_start_interval(10);
+
+    // payout the epoch coins to the buyer address
+    // 45706f6368436f696e is EpochCoin in hex
+	const epochCoinsAssetName = S.AssetName.new(Buffer.from("45706f6368436f696e", "hex"));
+    txBuilder.add_mint_asset_and_output_min_required_coin(
+        getMintingNativeScript(),
+        epochCoinsAssetName,
+        S.Int.new_i32(epochCoinAmount),
+        buyerAddress);
+
+    // payout lovelace to minting address
+    txBuilder.add_output_coin(
+        mintingAddress,
+        S.BigNum.from_str(lovelace.toString())
+    );
+
+    const transactionUnspentOutputs = S.TransactionUnspentOutputs.new();
+    req.body.utxosHex.forEach((u => transactionUnspentOutputs.add(S.TransactionUnspentOutput.from_bytes(Buffer.from(u, "hex")))));
+    txBuilder.add_inputs_from(transactionUnspentOutputs, S.CoinSelectionStrategyCIP2.LargestFirst);
+
+    txBuilder.add_change_if_needed(buyerAddress);
+
+    const transaction = S.Transaction.new(txBuilder.build(), S.TransactionWitnessSet.new());
+    const transactionBytes = Buffer.from(transaction.to_bytes(), "hex").toString("hex");
+    res.send(transactionBytes)
+});
+
 app.get('/getsignedtx', (req, res) => {
     const transaction = S.Transaction.from_bytes(Buffer.from(req.query.txBytes, "hex"))
     const witnessSet = S.TransactionWitnessSet.from_bytes(Buffer.from(req.query.witness, "hex"))
@@ -101,6 +153,25 @@ app.get('/getsignedtx', (req, res) => {
     res.send(bsignedTx)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+app.get('/getmultisignedtx', (req, res) => {
+    const transaction = S.Transaction.from_bytes(Buffer.from(req.query.txBytes, "hex"))
+    const witnessSet = S.TransactionWitnessSet.from_bytes(Buffer.from(req.query.witness, "hex"))
+    // need to pull the vkeys out in order to modify them.
+    // calling witnessSet.vkeys().add(...) does not change the contents
+    const vkeys = witnessSet.vkeys();
+    vkeys.add(
+        S.make_vkey_witness(
+            S.hash_transaction(transaction.body()),
+            S.PrivateKey.from_bech32(MINTING_SKEY_BECH32)));
+    witnessSet.set_vkeys(vkeys);
+    const scripts = S.NativeScripts.new();
+    scripts.add(getMintingNativeScript());
+    witnessSet.set_native_scripts(scripts);
+    const signedTx = S.Transaction.new(transaction.body(), witnessSet)
+    const bsignedTx = Buffer.from(signedTx.to_bytes(), "hex").toString("hex")
+    res.send(bsignedTx)
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
 });
--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ import { getAddress, getUtxos, getBalance} from "./walletUtils"
 const policyId = "424deb9056d16add0ae37cc654f8f4ae17e99efa9dd9fe5f8df1823c";
 
 async function getWalletStats() {
+    // TODO: should probably have a separate event for "connecting wallet" and getting stats.
     const isEnabled = await cardano.enable()
     if (!isEnabled) {
         return;
@@ -20,6 +21,8 @@ async function getWalletStats() {
     setNuggetSauceDropdowns(balance);
     $("#dip-button").prop("disabled", false);
     $("#payment-button").prop("disabled", false);
+    $("#epoch-coin-button").prop("disabled", false);
+    $("#epoch-coin-input").prop("disabled", false);
 
     console.log(await getUtxos());
  }
@@ -56,54 +59,94 @@ function setNuggetSauceDropdowns(balance) {
 }
 
 async function initiatePayment() {
-    //TODO ensure wallet is connected to expected network
+    try {
+        //TODO ensure wallet is connected to expected network
 
-    const address = await cardano.getChangeAddress()
-    // TODO: handle pagination
-    const utxosHex = await cardano.getUtxos();
-    const lovelace = parseInt(document.getElementById("lovelace-input").value);
+        const address = await cardano.getChangeAddress()
+        // TODO: handle pagination
+        const utxosHex = await cardano.getUtxos();
+        const lovelace = parseInt(document.getElementById("lovelace-input").value);
 
-    const datatosend = { address, lovelace, utxosHex }
-    const payload = JSON.stringify(datatosend)
-    const response = await fetch("/payVendingMachine", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: payload
-    });
-    const tx = await response.text()
-    await completeTransaction(tx);
+        const datatosend = { address, lovelace, utxosHex }
+        const payload = JSON.stringify(datatosend)
+        const response = await fetch("/payVendingMachine", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: payload
+        });
+        const tx = await response.text()
+        await completeTransaction(tx);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 async function initiateDip() {
-    //TODO ensure wallet is connected to expected network
+    try {
+        //TODO ensure wallet is connected to expected network
+        
+        const address = await cardano.getChangeAddress()
+        // TODO: handle pagination
+        const utxosHex = await cardano.getUtxos();
+        const nuggetSelection = $("#nugget-input").val()
+        const sauceSelection = $("#sauce-input").val()
+        const nuggetHex = a2hex(nuggetSelection);
+        const sauceHex = a2hex(sauceSelection);
+        
+        const datatosend = {
+            address,
+            utxosHex,
+            policyHex: policyId,
+            nuggetHex: nuggetHex,
+            sauceHex: sauceHex 
+        };
+        const payload = JSON.stringify(datatosend)
+        const response = await fetch("/dip", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: payload
+        });
+        const tx = await response.text()
+        await completeTransaction(tx);
+    } catch (e) {
+        console.log(e)
+    }
+}
 
-    const address = await cardano.getChangeAddress()
-    // TODO: handle pagination
-    const utxosHex = await cardano.getUtxos();
-    const nuggetSelection = $("#nugget-input").val()
-    const sauceSelection = $("#sauce-input").val()
-    const nuggetHex = a2hex(nuggetSelection);
-    const sauceHex = a2hex(sauceSelection);
+async function initiateEpochCoinBuy() {
+    try {
+        //TODO ensure wallet is connected to expected network
 
-    const datatosend = {
-        address,
-        utxosHex,
-        policyHex: policyId,
-        nuggetHex: nuggetHex,
-        sauceHex: sauceHex 
-    };
-    const payload = JSON.stringify(datatosend)
-    const response = await fetch("/dip", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: payload
-    });
-    const tx = await response.text()
-    await completeTransaction(tx);
+        const address = await cardano.getChangeAddress()
+        // TODO: handle pagination
+        const utxosHex = await cardano.getUtxos();
+        const epochCoinAmount = parseInt($("#epoch-coin-input").val())
+
+        const datatosend = {
+            address,
+            utxosHex,
+            epochCoinAmount
+        };
+        const payload = JSON.stringify(datatosend)
+        const response = await fetch("/buyEpochCoins", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: payload
+        });
+        const tx = await response.text()
+        const witnessSet = await cardano.signTx(tx);
+        const signResponse = await fetch("/getmultisignedtx?txBytes=" + tx + "&witness=" + witnessSet)
+        const signedTx = await signResponse.text()
+        await cardano.submitTx(signedTx);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 async function completeTransaction(tx) {
@@ -117,4 +160,9 @@ window.addEventListener('load', (event) => {
     $("#get-stats-button").on("click", () => getWalletStats());
     $("#payment-button").on("click", () => initiatePayment());
     $("#dip-button").on("click", () => initiateDip());
+    $("#epoch-coin-button").on("click", () => initiateEpochCoinBuy());
+    cardano.onNetworkChange(networkId => {
+        console.log(networkId);
+        getWalletStats();
+    })
 });
